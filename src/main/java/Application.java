ed1d438096e566d15a63824ea48587b16bb47086
Владимир Петько <petko.v@yandex.ru>
2021-11-04 18:21:24 +0600

    add database connection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Application {
     public static void main(String[] args) {
+        try {
+            Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tasks", "postgres", "postgres");
+        if (con != null){
+            System.out.println("connection ok");
+        }else{
+            System.out.println("no connection");
+        }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 
